9556800df73fc494ddfc3e8d157882cbaa77a937
Nginx Ninja <dummy.kenmen3@gmail.com>
2021-01-21 06:43:00 +0800

    Committing the Chapter:     66 exercise.

---
+++ 00_my_codes/section04/app_66.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Student(Resource):
+    def get(self, name):
+        return {'student': name}
+
+# The second argument is the endpoint that we are going to access from the class.
+# The second argument is the same thing as defining the Flask decorator's endpoint.
+api.add_resource(Student, '/student/<string:name>') # ex. http://127.0.0.1:5000/student/Rolf
+
+app.run(port=5000)
