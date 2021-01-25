4c0a1b69dba42dd9cdd94df7721c1f43678cb852
Nginx Ninja <dummy.kenmen3@gmail.com>
2021-01-25 09:18:59 +0800

    Committing the Chapter/s: 73, 74, 75, 76 exercise/s.

---
+++ 00_my_codes/section04/app_74.py
@@ -0,0 +1,66 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security_72 import authenticate, identity
+
+app = Flask(__name__)
+app.secret_key = "jose"
+api = Api(app)
+
+items = []
+
+jwt = JWT(app, authenticate, identity) #  The JWT will produce a new endpoint http://localhost/auth.
+
+class Item(Resource):
+    @jwt_required()
+    def get(self, name):
+        # original codes
+        # for item in items:
+        #     if item['name'] == name:
+        #         return item
+        item = next(filter(lambda item: item["name"] == name, items), None)
+
+        # return the 200 if the 'item' var is truthy (has value), else if falsy or None result.
+        return {"item": item}, 200 if item else 404
+
+    def post(self, name):
+        # The condition will execute if the item name has already exists.
+        # Status code 400 means a 'BAD REQUEST'
+        if next(filter(lambda item: item["name"] == name, items), None):
+            return {"message" : "An item with the name '{}' already exists.".format(name)}, 400
+
+        data = request.get_json()
+        item = {"name": name, "price": data["price"]}
+        items.append(item)
+        return item, 201
+
+    def delete(self, name):
+        global items # This will use the var 'items' list from above.
+        # This is going to re-create the 'items' list, excluding the var 'name' being called to delete.
+        items = list(filter(lambda item: item["name"] != name, items))
+        return {"message": "Item deleted."}
+
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda item: item["name"] == name, items), None)
+        if item is None:
+            item = {"name": name, "price": data["price"]}
+            items.append(item)
+        else:
+            item.update(data)
+
+        return item
+
+class ItemList(Resource):
+    def get(self):
+        # Remember to always return a JSON value format, it's a dictionary type in Python.
+        # The 'items' variable here is a List type.
+        return {"items": items}
+
+# The second argument is the endpoint that we are going to access from the class.
+# The second argument is the same thing as defining the Flask decorator's endpoint. ex. @app.route('/sample/endpoint')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, "/items")
+
+app.run(port=5000, debug=True)
